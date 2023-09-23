882d75cad991257ff4b5a01c1fc7ff7d1f15af53
Brandon Yates <BTYates@student.fullsail.edu>
2023-09-23 17:08:07 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+        <title>LifeBlog | Home </title>
+</head>
+<body>
+        <div class="container">
+                <!-- navbar -->
+                <?php include( ROOT_PATH . '/includes/navbar.php') ?>
+
+                <!-- banner -->
+                <?php include( ROOT_PATH . '/includes/banner.php') ?>
+
+                <!-- Page content -->
+                <div class="content">
+                        <h2 class="content-title">Recent Articles</h2>
+                        <hr>
+                        <!-- more content still to come here ... -->
+                </div>
+
+                <!-- footer -->
+                <?php include( ROOT_PATH . '/includes/footer.php') ?>
+        </div>
+</body>
+</html>
