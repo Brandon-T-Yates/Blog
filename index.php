be3ab0abf5ab34496ee68b80c75c534d81091af1
Brandon Yates <BTYates@student.fullsail.edu>
2023-09-23 17:18:01 -0400

    getting articles in. CSS, and index updated

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php require_once('config.php') ?>
 <?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+<!-- Retrieve all posts from database  -->
+<?php $posts = getPublishedPosts(); ?>
+
         <title>LifeBlog | Home </title>
 </head>
 <body>
@@ -14,7 +18,20 @@
                 <div class="content">
                         <h2 class="content-title">Recent Articles</h2>
                         <hr>
-                        <!-- more content still to come here ... -->
+                        <?php foreach ($posts as $post): ?>
+                            <div class="post" style="margin-left: 0px;">
+                                    <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+                                    <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+                                            <div class="post_info">
+                                                    <h3><?php echo $post['title'] ?></h3>
+                                                    <div class="info">
+                                                            <span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+                                                            <span class="read_more">Read more...</span>
+                                                    </div>
+                                            </div>
+                                    </a>
+                            </div>
+                        <?php endforeach ?>
                 </div>
 
                 <!-- footer -->
